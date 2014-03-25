bf55815a92f5cfc3eeb50b69f52b92f073aa6b43
Morgan McKenzie <rmtm@uvic.ca>
2014-03-25 11:27:45 -0700

    Add js files Remove the gitignore that stops any javascript files from loading

---
+++ js/inits.js
@@ -0,0 +1,31 @@
+scene_data_row_pad = 4
+scene_data = {}
+scene_data.row_pad = 4
+scene_data.sphere_count = 14
+scene_data.sphere_data_rows = 4
+scene_data.group_data_rows = 1
+scene_data.group_count = 3
+scene_data.tree_root = 1
+scene_data.sphere_geometries = new Float32Array([
+-30,5,30,5,-30,10,30,5,-30,15,30,5,-30,20,30,5,-30,25,30,5,-22,3,30,3,-22,15,30,3,-22,27,30,3,-16,5,30,3,-16,11,30,3,-16,19,30,3,-16,25,30,3,-6,3,30,3,-18,15,30,1,0,0,0,0,0,0,0,0,])
+scene_data.sphere_colors = new Float32Array([
+0.75,0.75,0,0.75,0.75,0,0.75,0.75,0,0.75,0.75,0,0.75,0.75,0,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0,0.75,0.75,0.75,0,0,0,0,0,0,])
+scene_data.group_boxmin = new Float32Array([
+-35.0005,-0.000500,24.9995,-35.0005,-0.000500,24.9995,-19.0003,-0.000300,26.9997,0,0,0,])
+scene_data.group_boxmax = new Float32Array([
+-18.9997,30.0005,35.0005,-2.9997,30.0005,35.0005,-2.9997,28.0003,33.0003,0,0,0,])
+scene_data.group_children = new Float32Array([
+65535,65535,0,0,2,0,65535,65535,0,0,0,0,])
+scene_data.group_objects = new Float32Array([
+0,8,0,0,0,0,8,6,0,0,0,0,])
+scene_data.group_hitmiss = new Float32Array([
+2,2,0,0,65535,0,65535,65535,0,0,0,0,])
+scene_data.scene_center = [-21.714285, 14.142858, 30.000000]
+scene_data.scene_extent = 44.528015
+scene_data.eye = [-21.714285, 14.142858, -35.095604]
+scene_data.yaw = -0.000000
+scene_data.pitch = -0.000000
+scene_data.roll = 0.000000
+scene_data.fov = 0.698132
+scene_data.background = [0.187500, 0.187500, 0.187500]
+window.scene_data = scene_data
---
+++ js/one.js
@@ -0,0 +1,31 @@
+scene_data_row_pad = 2
+scene_data = {}
+scene_data.row_pad = 2
+scene_data.sphere_count = 2
+scene_data.sphere_data_rows = 1
+scene_data.group_data_rows = 1
+scene_data.group_count = 1
+scene_data.tree_root = 0
+scene_data.sphere_geometries = new Float32Array([
+0,0,-30,1,5,0,-30,1,])
+scene_data.sphere_colors = new Float32Array([
+0.75,0.75,0,0.75,0.75,0,])
+scene_data.group_boxmin = new Float32Array([
+-1.0001,-1.0001,-31.0001,0,0,0,])
+scene_data.group_boxmax = new Float32Array([
+6.0001,1.0001,-28.9999,0,0.000000,0,])
+scene_data.group_children = new Float32Array([
+65535,65535,0,-0.000000,0,0,])
+scene_data.group_objects = new Float32Array([
+0,2,0,-0.000000,0,0,])
+scene_data.group_hitmiss = new Float32Array([
+65535,65535,0,-0.000000,0,0,])
+scene_data.scene_center = [2.500000, 0.000000, -30.000000]
+scene_data.scene_extent = 7.000000
+scene_data.eye = [2.500000, 0.000000, -40.233315]
+scene_data.yaw = -0.000000
+scene_data.pitch = -0.000000
+scene_data.roll = 0.000000
+scene_data.fov = 0.698132
+scene_data.background = [0.187500, 0.187500, 0.187500]
+window.scene_data = scene_data
